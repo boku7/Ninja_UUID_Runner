642adf38b3501d082c30f546c9279c80c96880eb
Bobby Cooke <19784872+boku7@users.noreply.github.com>
2021-11-04 11:56:44 -0700

    Update and rename bin2uuid.py to bin2uuids.py

--- bin2uuids.py
+++ bin2uuids.py
@@ -1,4 +1,4 @@
-# Author: Bobby Cooke (0xBoku/boku/boku7) // SpiderLabs // https://twitter.com/0xBoku // github.com/boku7 // https://www.linkedin.com/in/bobby-cooke/ // https://0xboku.com
+# Author: Bobby Cooke (0xBoku/boku/boku7) // https://twitter.com/0xBoku // github.com/boku7 // https://www.linkedin.com/in/bobby-cooke/ // https://0xboku.com
 # Modified code from: https://blog.securehat.co.uk/process-injection/shellcode-execution-via-enumsystemlocala
 #!usr/bin/python3
 
